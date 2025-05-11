80fb33b02b3b30bc84838782170073cbdaff7f6f
byEduarda <albuquerque.zxl@gmail.com>
2025-05-11 10:49:25 -0300

    .

--- soma.test.js
+++ soma.test.js
@@ -1,7 +1,9 @@
+import soma from './soma'
+
 const soma = require('./soma')
 
 describe('Soma', () => {
   it('deverá somar dois números inteiros', () => {
-    //seu código aqui
+    expect(soma(2, 2)).toBe(4)
   })
 })
